16678d809abb00e796d40b38ce0f7aa7b4b07bee
Subhodeep Neogi <subhodeepneogi16@gmail.com>
2024-07-25 01:38:16 +0530

    Update fingerprint_client_TCP.py
    Added multiprocessing

--- Traditional Method Using TCP-UDP/Programs/fingerprint_client_TCP.py
+++ Traditional Method Using TCP-UDP/Programs/fingerprint_client_TCP.py
@@ -1,26 +1,49 @@
 import os
 import socket
+import cv2
+import pickle
+import bz2
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('localhost', 12345))
 
-path = input("Enter your image: ")
-file = open(path, 'rb')
-size = os.path.getsize(path)
+path = input("Enter the image path: ")
 
-image_data = file.read(size)
+sample = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
-# send the keypoints and descriptor to the server and not the image
-# save the keypoints and descriptors as pickles
-# compress the pickles
+sift = cv2.SIFT_create()
+keypoints, descriptor = sift.detectAndCompute(sample, None)
 
+keypoints_as_tuples = [(kp.pt, kp.size, kp.angle, kp.response, kp.octave, kp.class_id) for kp in keypoints]
+keypoints_data = pickle.dumps(keypoints_as_tuples)
+descriptor_data = pickle.dumps(descriptor)
+
+#Compression using Bz2
+with bz2.BZ2File('compressed_keypoints.pkl.bz2', 'wb', compresslevel=9) as f:
+    pickle.dump(keypoints_data, f)
+with bz2.BZ2File('compressed_descriptor.pkl.bz2', 'wb', compresslevel=9) as f:
+    pickle.dump(descriptor_data, f)
+
+#Sending keypoints data
+size = os.path.getsize(r'E:\ISRO\VSSC\Fingerprint_analysis\fingerprint using client server\compressed_keypoints.pkl.bz2')
 client.send(size.to_bytes(4, 'big'))
+file = open(r'E:\ISRO\VSSC\Fingerprint_analysis\fingerprint using client server\compressed_keypoints.pkl.bz2', 'rb')
+keypoints_data = file.read(size)
+client.send(keypoints_data)
+file.close()
 
-client.send(image_data)
-
+#Sending descriptor data
+size = os.path.getsize(r'E:\ISRO\VSSC\Fingerprint_analysis\fingerprint using client server\compressed_descriptor.pkl.bz2')
+print(size)
+client.send(size.to_bytes(4, 'big'))
+file = open(r'E:\ISRO\VSSC\Fingerprint_analysis\fingerprint using client server\compressed_descriptor.pkl.bz2', 'rb')
+descriptor_data = file.read(size)
+client.send(descriptor_data)
 file.close()
 
 data = client.recv(1024)
+print("massege received")
 print(data.decode())
 
 client.close()
