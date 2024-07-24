86446ce1869a48aa16b5b3d1ce0c0f0ee004b7d7
Subhodeep Neogi <subhodeepneogi16@gmail.com>
2024-07-25 01:37:18 +0530

    Update fingerprint_server_TCP.py
    Added multiprocessing part

--- Traditional Method Using TCP-UDP/Programs/fingerprint_server_TCP.py
+++ Traditional Method Using TCP-UDP/Programs/fingerprint_server_TCP.py
@@ -1,97 +1,90 @@
+import pickle
 import socket
 import cv2
 import os
+import bz2
+import multiprocessing
 
+def image_processing(client_socket):
+        # Receiving Keypoints data
+        size = client_socket.recv(2048)
+        keypoints = client_socket.recv(int.from_bytes(size, 'big'))
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 12345))
-print("Server binded")
+        f = bz2.decompress(keypoints)
+        keypoints_decompossed = pickle.loads(f)
+        keypoints_unpickled = pickle.loads(keypoints_decompossed)
 
-# put a while loop which terminates on keyboard interrupt
-# inside the body of the while loop put all the processing before sever.close()
+        keypoints_1 = [
+            cv2.KeyPoint(
+                x=pt[0][0], y=pt[0][1], size=pt[1],
+                angle=pt[2], response=pt[3],
+                octave=pt[4], class_id=pt[5],
+            ) for pt in keypoints_unpickled if isinstance(pt[0], (tuple, list))
+        ]
+        keypoints_1 = tuple(keypoints_1)
 
-server.listen()
+        # Receiving descriptor data
+        size = client_socket.recv(8)
+        data_size = int.from_bytes(size, 'big')
+        descriptors = client_socket.recv(data_size)
+        print("received")
+        f = bz2.decompress(descriptors)
+        decomposed = pickle.loads(f)
+        descriptors_1 = pickle.loads(decomposed)
 
-# fork a child process for serving multiple clients
+        fingerprint_folder = r'E:\ISRO\VSSC\Fingerprint_analysis\Real_new'
 
-print("Now Listening")
+        best_score = 0
+        filename = None
+        best_image = None
+        kp1, kp2, mp = None, None, None
 
-client_socket, client_address = server.accept()
+        sift = cv2.SIFT_create()
+        matcher = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10}, {})
 
-size = client_socket.recv(2048)
+        for file in os.listdir(fingerprint_folder):
+            fingerprint_image_path = os.path.join(fingerprint_folder, file)
 
-file = open('request_image.BMP','wb')
-image_data = client_socket.recv(int.from_bytes(size, 'big'))
+            fingerprint_image = cv2.imread(fingerprint_image_path, cv2.IMREAD_GRAYSCALE)
+            if fingerprint_image is None:
+                print(f"Failed to load fingerprint image: {fingerprint_image_path}")
+                continue
 
-file.write(image_data)
+            keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)
+            matches = matcher.knnMatch(descriptors_1, descriptors_2, k=2)
 
-file.close()
+            match_points = []
+            for p, q in matches:
+                if p.distance < 0.1 * q.distance:
+                    match_points.append(p)
 
+            keypoints = min(len(keypoints_1), len(keypoints_2))
+            score = len(match_points) / keypoints * 100 if keypoints > 0 else 0
 
-fingerprint_folder = r'E:\ISRO\VSSC\Fingerprint_analysis\Real_new'
-sample_image_path = r'E:\ISRO\VSSC\Fingerprint_analysis\fingerprint using client server\request_image.BMP'
+            if score > best_score:
+                best_score = score
+                filename = file
+                best_image = fingerprint_image
+                kp1, kp2, mp = keypoints_1, keypoints_2, match_points
 
-sample = cv2.imread(sample_image_path, cv2.IMREAD_GRAYSCALE)
-print("image loaded")
-if sample is None:
-    print(f"Failed to load sample image: {sample_image_path}")
-    exit(1)
+        print(f"BEST MATCH: {filename}")
+        print(f"SCORE: {best_score}")
 
-best_score = 0
-filename = None
-best_image = None
-kp1, kp2, mp = None, None, None
+        if best_image is not None:
+            client_socket.send(b'Access Granted')
 
-sift = cv2.SIFT_create()
+        if filename is None:
+            client_socket.send(b'Access Denied')
 
-keypoints_1, descriptors_1 = sift.detectAndCompute(sample, None)
+if __name__=='__main__':
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 12345))
+    print("Server binded")
+    while True:
+        server.listen()
+        print("Now Listening")
+        client_socket, client_address = server.accept()
+        process = multiprocessing.Process(target=image_processing, args=(client_socket, ))
+        process.start()
 
-for file in os.listdir(fingerprint_folder):
-    fingerprint_image_path = os.path.join(fingerprint_folder, file)
-
-    fingerprint_image = cv2.imread(fingerprint_image_path, cv2.IMREAD_GRAYSCALE)
-    if fingerprint_image is None:
-        print(f"Failed to load fingerprint image: {fingerprint_image_path}")
-        continue
-
-    # implement the below line on the client side
-    keypoints_2, descriptors_2 = sift.detectAndCompute(fingerprint_image, None)
-
-    matcher = cv2.FlannBasedMatcher({'algorithm': 1, 'trees': 10}, {})
-    matches = matcher.knnMatch(descriptors_1, descriptors_2, k=2)
-
-    match_points = []
-    for p, q in matches:
-        if p.distance < 0.1 * q.distance:
-            match_points.append(p)
-
-    keypoints = min(len(keypoints_1), len(keypoints_2))
-    score = len(match_points) / keypoints * 100 if keypoints > 0 else 0
-
-
-    if score > best_score:
-        best_score = score
-        filename = file
-        best_image = fingerprint_image
-        kp1, kp2, mp = keypoints_1, keypoints_2, match_points
-
-
-print(f"BEST MATCH: {filename}")
-print(f"SCORE: {best_score}")
-
-if best_image is not None:
-    result = cv2.drawMatches(sample, kp1, best_image, kp2, mp, None)
-    result = cv2.resize(result, None, fx=4, fy=4)
-    cv2.imshow("Result", result)
-    cv2.waitKey(2000)
-    cv2.destroyAllWindows()
-    client_socket.send(b'Access Granted')
-
-if filename is None:
-    result_none = cv2.resize(sample, None, fx=4, fy=4)
-    client_socket.send(b'Access Denied')
-    cv2.imshow("Result",result_none)
-    cv2.waitKey(2000)
-    cv2.destroyAllWindows()
-
-server.close()
+    server.close()
